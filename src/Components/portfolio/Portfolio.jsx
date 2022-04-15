196ec0cdf60f21b2a447b7e0216b45db28d1409a
Sankku <santiagoemprendedor9@gmail.com>
2022-04-15 16:19:00 -0300

    New Portfolio Item

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -7,52 +7,60 @@ import IMG4 from "../../Assets/portfolio4.png";
 import IMG5 from "../../Assets/portfolio5.png";
 import IMG6 from "../../Assets/portfolio6.png";
 import IMG7 from "../../Assets/portfolio7.png";
+import IMG8 from "../../Assets/portfolio8.png";
 
 const data = [
   {
     id: 1,
+    image: IMG8,
+    title: "JavaScript Fighting Game",
+    github: "https://github.com/Sankku/Fighting-Game",
+    demo: "https://gleeful-syrniki-14341c.netlify.app",
+  },
+  {
+    id: 2,
     image: IMG1,
     title: "Only HTML, Css, JavaScript Portfolio",
     github: "https://github.com/Sankku/Portfolio",
     demo: "https://sankku.github.io/Portfolio/",
   },
   {
-    id: 2,
+    id: 3,
     image: IMG7,
     title: "React Restaurant Web Modern Ui",
     github: "https://github.com/Sankku/restaurant-web",
     demo: "https://restaurant-template-taupe.vercel.app/",
   },
   {
-    id: 3,
+    id: 4,
     image: IMG2,
     title: "JavaScript Admin Dashboard",
     github: "https://github.com/Sankku/Admin-Dashboard",
     demo: "https://sankku.github.io/Admin-Dashboard/",
   },
   {
-    id: 4,
+    id: 5,
     image: IMG3,
     title: "React Phone Weather Application",
     github: "https://github.com/Sankku/Weather-React",
     demo: "https://angry-bose-1663c7.netlify.app/",
   },
   {
-    id: 5,
+    id: 6,
     image: IMG5,
     title: "JavasCript Password Generator",
     github: "https://github.com/Sankku/Practical-React",
     demo: "https://peaceful-mahavira-01f523.netlify.app/",
   },
   {
-    id: 6,
+    id: 7,
     image: IMG4,
     title: "React ToDo Application",
     github: "https://github.com/Sankku/Practical-React",
     demo: "https://peaceful-mahavira-01f523.netlify.app/",
   },
   {
-    id: 7,
+    id: 8,
     image: IMG6,
     title: "JavaScript Counter",
     github: "https://github.com/Sankku/Practical-React",

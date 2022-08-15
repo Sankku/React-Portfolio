0a597f5747c162b1f5436a4965799599985eaf81
Sankku <santiagoemprendedor9@gmail.com>
2022-08-15 13:52:13 -0300

    Animations and card

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -2,28 +2,33 @@ import React from "react";
 import "./header.css";
 import CTA from "./CTA";
 import HeaderSocials from "./HeaderSocials";
+import MEMOJI from "../../assets/Memoji.png";
 
 const Header = () => {
   return (
     <header id="Home">
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Santiago Alaniz</h1>
-        <h5 className="text-light">
-          <b>I am a junior developer raised in Argentina</b>, and a university
-          student excited to find a place to expand my knowledge and keep
-          growing as a person and professional.
-        </h5>
-        <CTA />
+        <article className="header__card">
+          <div className="card__tittle">
+            <h5>Hello I'm</h5>
+          </div>
+          <div className="memoji">
+            <img src={MEMOJI} alt="" />
+          </div>
+          <h1>Santiago Alaniz</h1>
+          <h5 className="text-light">
+            <b>I am a junior developer raised in Argentina</b>, and a university
+            student excited to find a place to expand my knowledge and keep
+            growing as a person and professional.
+          </h5>
+          <CTA />
+          <div className="scroll__cont">
+            <a href="#contact" className="scroll__down">
+              Scroll Down ---&gt;
+            </a>
+          </div>
+        </article>
         <HeaderSocials />
-        {/* <div className="me">
-          <img src={ME} alt="me" />
-        </div> */}
-        <div>
-          <a href="#contact" className="scroll__down">
-            Scroll Down!
-          </a>
-        </div>
       </div>
     </header>
   );
